368041b46155c0ca2d8b358db24d0367203d87fd
Wei Hong <zxc37984@gmail.com>
2023-07-22 23:02:23 +0800

    refactor: use webpack to build

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path';
+
+export default {
+  target: 'node',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve('./', 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {presets: ['@babel/preset-env']},
+          },
+          {loader: 'ts-loader'},
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
