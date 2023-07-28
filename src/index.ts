58da585f09a2a7b44a885ec2657215d898611d17
Wei Hong <zxc37984@gmail.com>
2023-07-29 00:10:49 +0800

    feat: add horae class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello Horae!');
+import { ConfigType } from './constants';
+import { Config } from './type';
+
+class Horae {
+  config: Config = {
+    name: 'config',
+  };
+
+  constructor(name: string, type: ConfigType = ConfigType.JSON) {
+    this.config.name = name;
+    this.config.type = type;
+  }
+}
+
+export { Horae };
---
+++ src/type.ts
@@ -0,0 +1,6 @@
+import { ConfigType } from './constants';
+
+export type Config = {
+  name: string;
+  type?: ConfigType;
+};
