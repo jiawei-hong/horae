1096e57d99eb2565eaedf957658966ecac45ff5d
Wei Hong <zxc37984@gmail.com>
2026-03-24 09:15:24 +0800

    refactor: move mockConfig inline and remove constants/mock.ts

--- src/__tests__/has.test.ts
+++ src/__tests__/has.test.ts
@@ -1,6 +1,12 @@
 import fs from 'fs';
 import { Horae } from '..';
-import { mockConfig } from '../constants/mock';
+
+const mockConfig = {
+  name: 'horae',
+  type: 'json',
+  position: { x: 100, y: 200 },
+  a: { b: { c: { d: 100 } } },
+};
 
 jest.mock('fs');
 
